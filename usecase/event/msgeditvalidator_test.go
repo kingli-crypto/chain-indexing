a85e5ab8a9e7c4531dd64e49d110babed37b7fea
jongwhan <jonghwan@crypto.com>
2020-11-23 12:56:53 +0800

    add two tests

---
+++ usecase/event/msgeditvalidator_test.go
@@ -0,0 +1,26 @@
+package event_test
+
+import (
+	event_entity "github.com/crypto-com/chainindex/entity/event"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	event_usecase "github.com/crypto-com/chainindex/usecase/event"
+)
+
+var _ = Describe("Event", func() {
+	registry := event_entity.NewRegistry()
+	event_usecase.RegisterEvents(registry)
+
+	Describe("En/DecodeMsgEditValidator", func() {
+		It("should able to encode and decode to the same event", func() {
+
+			Expect(1).To(Equal(1))
+		})
+
+		It("should able to encode and decode to failed event", func() {
+
+			Expect(1).To(Equal(1))
+		})
+	})
+})

705a1b075816058309b24d052f8be4dcbf0acbec
jongwhan <jonghwan@crypto.com>
2020-11-23 18:20:51 +0800

    decode ok

--- usecase/event/msgcreatevalidator_test.go
+++ usecase/event/msgcreatevalidator_test.go
@@ -5,7 +5,10 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/crypto-com/chainindex/usecase/coin"
+	"github.com/crypto-com/chainindex/usecase/event"
 	event_usecase "github.com/crypto-com/chainindex/usecase/event"
+	"github.com/crypto-com/chainindex/usecase/model"
 )
 
 var _ = Describe("Event", func() {
@@ -14,6 +17,35 @@ var _ = Describe("Event", func() {
 
 	Describe("En/DecodeMsgCreateValidator", func() {
 		It("should able to encode and decode to the same event", func() {
+			commiossionrates := model.CommissionRates{
+				Rate:          "0.100000000000000000",
+				MaxRate:       "0.200000000000000000",
+				MaxChangeRate: "0.010000000000000000",
+			}
+
+			event := event_usecase.NewMsgCreateValidator(
+				event.MsgCommonParams{
+					BlockHeight: int64(503978),
+					TxHash:      "E69985AC8168383A81B7952DBE03EB9B3400FF80AEC0F362369DD7F38B1C2FE9",
+					TxSuccess:   true,
+					MsgIndex:    0,
+				},
+				model.MsgCreateValidatorParams{
+					CommissionRates:  commiossionrates,
+					DelegatorAddress: "tcro1fmprm0sjy6lz9llv7rltn0v2azzwcwzvk2lsyn",
+					ValidatorAddress: "tcrocncl1fmprm0sjy6lz9llv7rltn0v2azzwcwzvr4ufus",
+					PubKey:           "tcrocnclconspub1zcjduepqa5rksn4ds9u6jmmg4n86d9wct7wmj23pyqe6p7e252lffzqsgcvqxm5lc2",
+					Amount:           coin.MustNewCoinFromString("10"),
+				},
+			)
+			encoded, err := event.ToJSON()
+			Expect(err).To(BeNil())
+
+			decodedEvent, err := registry.DecodeByType(
+				event_usecase.MSG_CREATE_VALIDATOR, 1, []byte(encoded),
+			)
+			Expect(err).To(BeNil())
+			Expect(decodedEvent).To(Equal(event))
 
 			Expect(1).To(Equal(1))
 		})

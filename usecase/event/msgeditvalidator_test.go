7f9609cdca78ec479a2c321fa05ba3f193136f93
jongwhan <jonghwan@crypto.com>
2020-11-23 18:53:23 +0800

    edit validator decode, encode ok

--- usecase/event/msgeditvalidator_test.go
+++ usecase/event/msgeditvalidator_test.go
@@ -5,7 +5,9 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/crypto-com/chainindex/usecase/event"
 	event_usecase "github.com/crypto-com/chainindex/usecase/event"
+	"github.com/crypto-com/chainindex/usecase/model"
 )
 
 var _ = Describe("Event", func() {
@@ -14,6 +16,38 @@ var _ = Describe("Event", func() {
 
 	Describe("En/DecodeMsgEditValidator", func() {
 		It("should able to encode and decode to the same event", func() {
+
+			event := event_usecase.NewMsgEditValidator(
+				event.MsgCommonParams{
+					BlockHeight: int64(503978),
+					TxHash:      "E69985AC8168383A81B7952DBE03EB9B3400FF80AEC0F362369DD7F38B1C2FE9",
+					TxSuccess:   true,
+					MsgIndex:    0,
+				},
+				model.MsgEditValidatorParams{
+					ValidatorAddress:  "tcrocncl1fmprm0sjy6lz9llv7rltn0v2azzwcwzvr4ufus",
+					CommissionRate:    "0.1",
+					MinSelfDelegation: "2",
+				},
+			)
+
+			encoded, err := event.ToJSON()
+			Expect(err).To(BeNil())
+
+			decodedEvent, err := registry.DecodeByType(
+				event_usecase.MSG_EDIT_VALIDATOR, 1, []byte(encoded),
+			)
+			Expect(err).To(BeNil())
+			Expect(decodedEvent).To(Equal(event))
+			typedEvent, _ := decodedEvent.(*event_usecase.MsgEditValidator)
+			Expect(typedEvent.Name()).To(Equal(event_usecase.MSG_EDIT_VALIDATOR_CREATED))
+			Expect(typedEvent.Version()).To(Equal(1))
+
+			Expect(typedEvent.TxHash).To(Equal(event.TxHash))
+			Expect(typedEvent.MsgIndex).To(Equal(event.MsgIndex))
+			Expect(typedEvent.ValidatorAddress).To(Equal(event.ValidatorAddress))
+			Expect(typedEvent.CommissionRate).To(Equal(event.CommissionRate))
+			Expect(typedEvent.MinSelfDelegation).To(Equal(event.MinSelfDelegation))
 
 			Expect(1).To(Equal(1))
 		})
